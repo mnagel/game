c6077a786069fed5925040bcb49512904340fa31
Philipp Kern <phil@philkern.de>
2020-05-09 12:15:41 +0200

    Add a central entrypoint
    This allows to gate client vs server later on.

---
+++ src/entry/entry.gd
@@ -0,0 +1,14 @@
+extends Node
+
+# Entry point for the whole app
+# Determine the type of app this is, and load the entry point for that type
+func _ready():
+	print("Application started")
+	var menu = "res://scenes/player-menu.tscn"
+	if OS.has_feature("client"):
+		print("Is client")
+		get_tree().change_scene(menu)
+	# When running from the editor, this is how we'll default to being a client
+	else:
+		print("Could not detect application type! Defaulting to client.")
+		get_tree().change_scene(menu)
