65b9f35cf4fe593ab645cac9ad61caa24cabb96b
Fabian Knittel <fabian.knittel@lettink.de>
2020-05-10 01:32:37 +0200

    publish HTML5 to GitHub pages: fetch missing branch first

--- export/export-to-gh-pages.sh
+++ export/export-to-gh-pages.sh
@@ -8,10 +8,7 @@ repo_uri="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}
 git config user.name "$GITHUB_ACTOR"
 git config user.email "${GITHUB_ACTOR}@bots.github.com"
 
-pwd
-ls -la
-ls -la "$GITHUB_WORKSPACE"
-git status
+git fetch origin refs/remotes/origin/gh-pages
 git checkout --force -B gh-pages --track refs/remotes/origin/gh-pages
 
 rsync -av --delete --cvs-exclude "${content_dir}/" ./
