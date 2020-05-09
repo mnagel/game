cc9b89f0f5ff67e100f44d33242967f45be3e141
Philipp Kern <phil@philkern.de>
2020-05-09 12:58:21 +0200

    Server: Listen for incoming connections

--- src/server/server-entry.gd
+++ src/server/server-entry.gd
@@ -1,6 +1,22 @@
 extends Node
 
-# Do any server specific setup here
-# Then open a lobby and start listening for users
+const SERVER_PORT := 3333
+const MAX_CLIENTS := 25
+
+def _player_connected(id):
+	# Purely informational console output.
+	print("Player connected: " + str(id))
+
+# Open the port for listening, then open up a lobby.
 func _ready():
-	get_tree().change_scene("res://server/lobby/server-lobby.tscn")
+	var peer = NetworkedMultiplayerENet.new()
+	var result = peer.create_server(SERVER_PORT, MAX_CLIENTS)
+	if result == OK:
+		get_tree().set_network_peer(peer)
+		get_tree().connect("network_peer_connected", self, "_player_connected")
+		print("Server started.")
+		get_tree().change_scene("res://server/lobby/server-lobby.tscn")
+		return true
+	else:
+		print("Failed to start server: %d" % result)
+		return false
