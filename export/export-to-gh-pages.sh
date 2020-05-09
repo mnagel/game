83ffdca3fe41517021afd0f6d9f1a9992b8864d5
Fabian Knittel <fabian.knittel@lettink.de>
2020-05-10 01:21:43 +0200

    publish HTML5 to GitHub pages: attempt more explicit checkout

--- export/export-to-gh-pages.sh
+++ export/export-to-gh-pages.sh
@@ -8,7 +8,7 @@ repo_uri="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}
 git config user.name "$GITHUB_ACTOR"
 git config user.email "${GITHUB_ACTOR}@bots.github.com"
 
-git checkout gh-pages
+git checkout --force -B gh-pages --track refs/remotes/origin/gh-pages
 
 rsync -av --delete --cvs-exclude "${content_dir}/" ./
 git add .
