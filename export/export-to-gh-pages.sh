b46dd7609667f5394ea0c801f5c98c09d0c69c00
Fabian Knittel <fabian.knittel@lettink.de>
2020-05-10 01:15:07 +0200

    Publish HTML5 to GitHub pages

---
+++ export/export-to-gh-pages.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -eux
+
+content_dir="$1"
+repo_uri="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+
+git config user.name "$GITHUB_ACTOR"
+git config user.email "${GITHUB_ACTOR}@bots.github.com"
+
+git fetch --no-tags origin gh-pages:refs/remotes/origin/gh-pages
+git checkout --force -B gh-pages --track refs/remotes/origin/gh-pages
+
+rsync -av --delete --cvs-exclude "${content_dir}/" ./
+git add .
+
+if ! git commit -m "updated GitHub Pages"; then
+    echo "nothing to commit"
+    exit 0
+fi
+
+git remote set-url origin "$repo_uri"
+git push origin gh-pages
