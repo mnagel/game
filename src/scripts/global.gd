cccc86f7c0e0f5137541a46b4141561666978556
Michael Nagel <ubuntu@nailor.devzero.de>
2020-05-24 22:47:10 +0200

    balancing fixes

--- src/scripts/global.gd
+++ src/scripts/global.gd
@@ -1,5 +1,7 @@
 extends Node
 
+var rng = RandomNumberGenerator.new()
+
 # Player's stuff
 var available_colors = [
 	"red",
@@ -45,30 +47,22 @@ var available_names = [
 	"Fooba"
 	]
 
-var rng = RandomNumberGenerator.new()
+# main game balancing
+const num_rounds = 4
+const stargfx_speed = 5
+const connection_maxlength = 50 # maximum distance between exploded stars
+const safezone_radius = 90  # minimum distance between the primary and secondary novas
+const stargfxs_count = 60
 
-# Stargfx related stuff
-var stargfx_speed = 5
-var connection_line_width = 7
-export (int) var stargfxs_count = 60
-
-var margin = 5
+# cosmetics
+const connection_line_width = 7
+var sound = true
+var antialiasing = true
 
 # Viewport limits
 var min_limits = Vector2(10, 10)
 var max_limits = Vector2(440, 440)
-
-# Anti things xD
-var antialiasing = true
-
-# Sfx stuff
-var sound = true
-
-# Rounds 
-const num_rounds = 1
-const connection_maxlength = 50 # maximum distance between exploded stars
-const safezone_radius = 90  # minimum distance between the primary and secondary novas
-
+var margin = 5
 
 func getRandomPosition():
 	var rx = rng.randi_range(min_limits.x + margin * 2, max_limits.x - margin * 2)
